7c99a812630825bd6656ccf025fe3fd4b8ee37e0
Jean-Louis Voiseux <48380853+jlvoiseux@users.noreply.github.com>
2025-01-12 17:11:26 +0200

    Cleanup SIMD span buffer processing

--- src/buffer/qkSpanBuffer.c
+++ src/buffer/qkSpanBuffer.c
@@ -178,176 +178,145 @@ void qkSpanBufferProcess8(qkBuffer* pSpanBuffer, int width, int height, uint32_t
 		const int spanWidth	  = endX - startX + 1;
 		const int pixelOffset = y * width + startX;
 
-		const float startZ = pSpanBuffer->pFloat0[i];
-		const float zStep  = (pSpanBuffer->pFloat1[i] - startZ) / (float)spanWidth;
+		const float startZ		= pSpanBuffer->pFloat0[i];
+		const float zStep		= (pSpanBuffer->pFloat1[i] - startZ) / (float)spanWidth;
+		const bool	perspective = pSpanBuffer->pInt1[i] != 0;
 
-		const bool perspective = pSpanBuffer->pInt1[i] != 0;
+		__m256		 zVec	  = _mm256_setr_ps(startZ, startZ + zStep, startZ + zStep * 2, startZ + zStep * 3, startZ + zStep * 4, startZ + zStep * 5, startZ + zStep * 6, startZ + zStep * 7);
+		const __m256 zStepVec = _mm256_set1_ps(zStep * 8.0f);
+
+		__m256 uVec, vVec;
+		__m256 uStepVec, vStepVec;
+		__m256 uOverZVec, vOverZVec, invZVec;
+		__m256 uOverZStepVec, vOverZStepVec, invZStepVec;
+
+		const float u	  = !perspective ? pSpanBuffer->pFloat3[i] / pSpanBuffer->pFloat5[i] : 0.0f;
+		const float v	  = !perspective ? pSpanBuffer->pFloat4[i] / pSpanBuffer->pFloat5[i] : 0.0f;
+		const float uStep = !perspective ? ((pSpanBuffer->pFloat6[i] / pSpanBuffer->pFloat8[i]) - u) / (float)spanWidth : 0.0f;
+		const float vStep = !perspective ? ((pSpanBuffer->pFloat7[i] / pSpanBuffer->pFloat8[i]) - v) / (float)spanWidth : 0.0f;
+
+		const float uOverZStep = perspective ? (pSpanBuffer->pFloat6[i] - pSpanBuffer->pFloat3[i]) / (float)spanWidth : 0.0f;
+		const float vOverZStep = perspective ? (pSpanBuffer->pFloat7[i] - pSpanBuffer->pFloat4[i]) / (float)spanWidth : 0.0f;
+		const float invZStep   = perspective ? (pSpanBuffer->pFloat8[i] - pSpanBuffer->pFloat5[i]) / (float)spanWidth : 0.0f;
 
 		if (!perspective)
 		{
-			const float u	  = pSpanBuffer->pFloat3[i] / pSpanBuffer->pFloat5[i];
-			const float v	  = pSpanBuffer->pFloat4[i] / pSpanBuffer->pFloat5[i];
-			const float uStep = ((pSpanBuffer->pFloat6[i] / pSpanBuffer->pFloat8[i]) - u) / (float)spanWidth;
-			const float vStep = ((pSpanBuffer->pFloat7[i] / pSpanBuffer->pFloat8[i]) - v) / (float)spanWidth;
-
-			__m256		 zVec	  = _mm256_setr_ps(startZ, startZ + zStep, startZ + zStep * 2, startZ + zStep * 3, startZ + zStep * 4, startZ + zStep * 5, startZ + zStep * 6, startZ + zStep * 7);
-			const __m256 zStepVec = _mm256_set1_ps(zStep * 8.0f);
-
-			__m256		 uVec	  = _mm256_setr_ps(u, u + uStep, u + uStep * 2, u + uStep * 3, u + uStep * 4, u + uStep * 5, u + uStep * 6, u + uStep * 7);
-			const __m256 uStepVec = _mm256_set1_ps(uStep * 8.0f);
-
-			__m256		 vVec	  = _mm256_setr_ps(v, v + vStep, v + vStep * 2, v + vStep * 3, v + vStep * 4, v + vStep * 5, v + vStep * 6, v + vStep * 7);
-			const __m256 vStepVec = _mm256_set1_ps(vStep * 8.0f);
-
-			for (int x = 0; x < spanWidth; x += 8)
-			{
-				const int remainingPixels = spanWidth - x;
-				if (remainingPixels < 8)
-					break;
-
-				const __m256 zBuffer = _mm256_load_ps(&pZBuffer[pixelOffset + x]);
-				const __m256 mask	 = _mm256_and_ps(_mm256_cmp_ps(zVec, _mm256_set1_ps(0.0f), _CMP_GT_OQ), _mm256_cmp_ps(zVec, zBuffer, _CMP_LT_OQ));
-
-				if (_mm256_movemask_ps(mask))
-				{
-					__m256i colors;
-					qkTextureSample8(pTex, uVec, vVec, &colors);
-
-					__m256i* pDest	  = (__m256i*)&pFrameBuffer[pixelOffset + x];
-					__m256i	 existing = _mm256_load_si256(pDest);
-					__m256i	 masked	  = _mm256_blendv_epi8(existing, colors, _mm256_castps_si256(mask));
-					_mm256_store_si256(pDest, masked);
-
-					_mm256_maskstore_ps(&pZBuffer[pixelOffset + x], _mm256_castps_si256(mask), zVec);
-				}
-
-				zVec = _mm256_add_ps(zVec, zStepVec);
-				uVec = _mm256_add_ps(uVec, uStepVec);
-				vVec = _mm256_add_ps(vVec, vStepVec);
-			}
+			uVec	 = _mm256_setr_ps(u, u + uStep, u + uStep * 2, u + uStep * 3, u + uStep * 4, u + uStep * 5, u + uStep * 6, u + uStep * 7);
+			vVec	 = _mm256_setr_ps(v, v + vStep, v + vStep * 2, v + vStep * 3, v + vStep * 4, v + vStep * 5, v + vStep * 6, v + vStep * 7);
+			uStepVec = _mm256_set1_ps(uStep * 8.0f);
+			vStepVec = _mm256_set1_ps(vStep * 8.0f);
 		}
 		else
 		{
-			const float uOverZStep = (pSpanBuffer->pFloat6[i] - pSpanBuffer->pFloat3[i]) / (float)spanWidth;
-			const float vOverZStep = (pSpanBuffer->pFloat7[i] - pSpanBuffer->pFloat4[i]) / (float)spanWidth;
-			const float invZStep   = (pSpanBuffer->pFloat8[i] - pSpanBuffer->pFloat5[i]) / (float)spanWidth;
+			uOverZVec	  = _mm256_setr_ps(pSpanBuffer->pFloat3[i],
+									   pSpanBuffer->pFloat3[i] + uOverZStep,
+									   pSpanBuffer->pFloat3[i] + uOverZStep * 2,
+									   pSpanBuffer->pFloat3[i] + uOverZStep * 3,
+									   pSpanBuffer->pFloat3[i] + uOverZStep * 4,
+									   pSpanBuffer->pFloat3[i] + uOverZStep * 5,
+									   pSpanBuffer->pFloat3[i] + uOverZStep * 6,
+									   pSpanBuffer->pFloat3[i] + uOverZStep * 7);
+			vOverZVec	  = _mm256_setr_ps(pSpanBuffer->pFloat4[i],
+									   pSpanBuffer->pFloat4[i] + vOverZStep,
+									   pSpanBuffer->pFloat4[i] + vOverZStep * 2,
+									   pSpanBuffer->pFloat4[i] + vOverZStep * 3,
+									   pSpanBuffer->pFloat4[i] + vOverZStep * 4,
+									   pSpanBuffer->pFloat4[i] + vOverZStep * 5,
+									   pSpanBuffer->pFloat4[i] + vOverZStep * 6,
+									   pSpanBuffer->pFloat4[i] + vOverZStep * 7);
+			invZVec		  = _mm256_setr_ps(pSpanBuffer->pFloat5[i],
+									   pSpanBuffer->pFloat5[i] + invZStep,
+									   pSpanBuffer->pFloat5[i] + invZStep * 2,
+									   pSpanBuffer->pFloat5[i] + invZStep * 3,
+									   pSpanBuffer->pFloat5[i] + invZStep * 4,
+									   pSpanBuffer->pFloat5[i] + invZStep * 5,
+									   pSpanBuffer->pFloat5[i] + invZStep * 6,
+									   pSpanBuffer->pFloat5[i] + invZStep * 7);
+			uOverZStepVec = _mm256_set1_ps(uOverZStep * 8.0f);
+			vOverZStepVec = _mm256_set1_ps(vOverZStep * 8.0f);
+			invZStepVec	  = _mm256_set1_ps(invZStep * 8.0f);
+		}
 
-			__m256		 zVec	  = _mm256_setr_ps(startZ, startZ + zStep, startZ + zStep * 2, startZ + zStep * 3, startZ + zStep * 4, startZ + zStep * 5, startZ + zStep * 6, startZ + zStep * 7);
-			const __m256 zStepVec = _mm256_set1_ps(zStep * 8.0f);
+		for (int x = 0; x < spanWidth; x += 8)
+		{
+			const int remainingPixels = spanWidth - x;
+			if (remainingPixels < 8)
+				break;
 
-			__m256		 uOverZVec	   = _mm256_setr_ps(pSpanBuffer->pFloat3[i],
-												pSpanBuffer->pFloat3[i] + uOverZStep,
-												pSpanBuffer->pFloat3[i] + uOverZStep * 2,
-												pSpanBuffer->pFloat3[i] + uOverZStep * 3,
-												pSpanBuffer->pFloat3[i] + uOverZStep * 4,
-												pSpanBuffer->pFloat3[i] + uOverZStep * 5,
-												pSpanBuffer->pFloat3[i] + uOverZStep * 6,
-												pSpanBuffer->pFloat3[i] + uOverZStep * 7);
-			const __m256 uOverZStepVec = _mm256_set1_ps(uOverZStep * 8.0f);
+			const __m256 zBuffer = _mm256_load_ps(&pZBuffer[pixelOffset + x]);
+			const __m256 mask	 = _mm256_and_ps(_mm256_cmp_ps(zVec, _mm256_set1_ps(0.0f), _CMP_GT_OQ), _mm256_cmp_ps(zVec, zBuffer, _CMP_LT_OQ));
 
-			__m256		 vOverZVec	   = _mm256_setr_ps(pSpanBuffer->pFloat4[i],
-												pSpanBuffer->pFloat4[i] + vOverZStep,
-												pSpanBuffer->pFloat4[i] + vOverZStep * 2,
-												pSpanBuffer->pFloat4[i] + vOverZStep * 3,
-												pSpanBuffer->pFloat4[i] + vOverZStep * 4,
-												pSpanBuffer->pFloat4[i] + vOverZStep * 5,
-												pSpanBuffer->pFloat4[i] + vOverZStep * 6,
-												pSpanBuffer->pFloat4[i] + vOverZStep * 7);
-			const __m256 vOverZStepVec = _mm256_set1_ps(vOverZStep * 8.0f);
-
-			__m256		 invZVec	 = _mm256_setr_ps(pSpanBuffer->pFloat5[i],
-											  pSpanBuffer->pFloat5[i] + invZStep,
-											  pSpanBuffer->pFloat5[i] + invZStep * 2,
-											  pSpanBuffer->pFloat5[i] + invZStep * 3,
-											  pSpanBuffer->pFloat5[i] + invZStep * 4,
-											  pSpanBuffer->pFloat5[i] + invZStep * 5,
-											  pSpanBuffer->pFloat5[i] + invZStep * 6,
-											  pSpanBuffer->pFloat5[i] + invZStep * 7);
-			const __m256 invZStepVec = _mm256_set1_ps(invZStep * 8.0f);
-
-			for (int x = 0; x < spanWidth; x += 8)
+			if (_mm256_movemask_ps(mask))
 			{
-				const int remainingPixels = spanWidth - x;
-				if (remainingPixels < 8)
-					break;
-
-				const __m256 zBuffer = _mm256_load_ps(&pZBuffer[pixelOffset + x]);
-				const __m256 mask	 = _mm256_and_ps(_mm256_cmp_ps(zVec, _mm256_set1_ps(0.0f), _CMP_GT_OQ), _mm256_cmp_ps(zVec, zBuffer, _CMP_LT_OQ));
-
-				if (_mm256_movemask_ps(mask))
+				__m256i colors;
+				if (!perspective)
+				{
+					qkTextureSample8(pTex, uVec, vVec, &colors);
+				}
+				else
 				{
 					const __m256 invZRecip = _mm256_rcp_ps(invZVec);
 					const __m256 uVec	   = _mm256_mul_ps(uOverZVec, invZRecip);
 					const __m256 vVec	   = _mm256_mul_ps(vOverZVec, invZRecip);
-
-					__m256i colors;
 					qkTextureSample8(pTex, uVec, vVec, &colors);
-
-					__m256i* pDest	  = (__m256i*)&pFrameBuffer[pixelOffset + x];
-					__m256i	 existing = _mm256_load_si256(pDest);
-					__m256i	 masked	  = _mm256_blendv_epi8(existing, colors, _mm256_castps_si256(mask));
-					_mm256_store_si256(pDest, masked);
-
-					_mm256_maskstore_ps(&pZBuffer[pixelOffset + x], _mm256_castps_si256(mask), zVec);
 				}
 
-				zVec	  = _mm256_add_ps(zVec, zStepVec);
+				__m256i* pDest	  = (__m256i*)&pFrameBuffer[pixelOffset + x];
+				__m256i	 existing = _mm256_load_si256(pDest);
+				__m256i	 masked	  = _mm256_blendv_epi8(existing, colors, _mm256_castps_si256(mask));
+				_mm256_store_si256(pDest, masked);
+				_mm256_maskstore_ps(&pZBuffer[pixelOffset + x], _mm256_castps_si256(mask), zVec);
+			}
+
+			zVec = _mm256_add_ps(zVec, zStepVec);
+			if (!perspective)
+			{
+				uVec = _mm256_add_ps(uVec, uStepVec);
+				vVec = _mm256_add_ps(vVec, vStepVec);
+			}
+			else
+			{
 				uOverZVec = _mm256_add_ps(uOverZVec, uOverZStepVec);
 				vOverZVec = _mm256_add_ps(vOverZVec, vOverZStepVec);
 				invZVec	  = _mm256_add_ps(invZVec, invZStepVec);
 			}
 		}
 
-		// Handle remaining pixels if any
 		const int remainingStart = (spanWidth / 8) * 8;
 		if (remainingStart < spanWidth)
 		{
-			// Process remaining pixels one by one using the non-SIMD path
-			float z = startZ + remainingStart * zStep;
-
+			float z = startZ + (float)remainingStart * zStep;
 			if (!perspective)
 			{
-				float		u	  = pSpanBuffer->pFloat3[i] / pSpanBuffer->pFloat5[i];
-				float		v	  = pSpanBuffer->pFloat4[i] / pSpanBuffer->pFloat5[i];
-				const float uStep = ((pSpanBuffer->pFloat6[i] / pSpanBuffer->pFloat8[i]) - u) / (float)spanWidth;
-				const float vStep = ((pSpanBuffer->pFloat7[i] / pSpanBuffer->pFloat8[i]) - v) / (float)spanWidth;
-
-				u += uStep * remainingStart;
-				v += vStep * remainingStart;
+				float currU = u + uStep * (float)remainingStart;
+				float currV = v + vStep * (float)remainingStart;
 
 				for (int x = remainingStart; x < spanWidth; x++)
 				{
 					if (z > 0.0f && z < pZBuffer[pixelOffset + x])
 					{
-						pFrameBuffer[pixelOffset + x] = qkTextureSample(pTex, u, v);
+						pFrameBuffer[pixelOffset + x] = qkTextureSample(pTex, currU, currV);
 						pZBuffer[pixelOffset + x]	  = z;
 					}
 					z += zStep;
-					u += uStep;
-					v += vStep;
+					currU += uStep;
+					currV += vStep;
 				}
 			}
 			else
 			{
-				float		uOverZ	   = pSpanBuffer->pFloat3[i];
-				float		vOverZ	   = pSpanBuffer->pFloat4[i];
-				float		invZ	   = pSpanBuffer->pFloat5[i];
-				const float uOverZStep = (pSpanBuffer->pFloat6[i] - uOverZ) / (float)spanWidth;
-				const float vOverZStep = (pSpanBuffer->pFloat7[i] - vOverZ) / (float)spanWidth;
-				const float invZStep   = (pSpanBuffer->pFloat8[i] - invZ) / (float)spanWidth;
-
-				uOverZ += uOverZStep * remainingStart;
-				vOverZ += vOverZStep * remainingStart;
-				invZ += invZStep * remainingStart;
+				float uOverZ = pSpanBuffer->pFloat3[i] + uOverZStep * (float)remainingStart;
+				float vOverZ = pSpanBuffer->pFloat4[i] + vOverZStep * (float)remainingStart;
+				float invZ	 = pSpanBuffer->pFloat5[i] + invZStep * (float)remainingStart;
 
 				for (int x = remainingStart; x < spanWidth; x++)
 				{
 					if (z > 0.0f && z < pZBuffer[pixelOffset + x])
 					{
 						const float invZRecip		  = 1.0f / invZ;
-						const float u				  = uOverZ * invZRecip;
-						const float v				  = vOverZ * invZRecip;
-						pFrameBuffer[pixelOffset + x] = qkTextureSample(pTex, u, v);
+						const float invU			  = uOverZ * invZRecip;
+						const float invV			  = vOverZ * invZRecip;
+						pFrameBuffer[pixelOffset + x] = qkTextureSample(pTex, invU, invV);
 						pZBuffer[pixelOffset + x]	  = z;
 					}
 					z += zStep;
